98d9c5e5aeec3c8a249e89b718e019be1831d8d0
= <gaston.borrazas@gmail.com>
2015-09-29 13:45:06 -0300

    Adding missing files

---
+++ src/common/actor/actor.c
@@ -0,0 +1,26 @@
+/**
+*
+*   ██████╗   ██╗ ███████╗ ███╗   ███╗ ██╗   ██╗
+*   ██╔══██╗ ███║ ██╔════╝ ████╗ ████║ ██║   ██║
+*   ██████╔╝ ╚██║ █████╗   ██╔████╔██║ ██║   ██║
+*   ██╔══██╗  ██║ ██╔══╝   ██║╚██╔╝██║ ██║   ██║
+*   ██║  ██║  ██║ ███████╗ ██║ ╚═╝ ██║ ╚██████╔╝
+*   ╚═╝  ╚═╝  ╚═╝ ╚══════╝ ╚═╝     ╚═╝  ╚═════╝
+*
+* @license GNU GENERAL PUBLIC LICENSE - Version 2, June 1991
+ *          See LICENSE file for further information
+*/
+
+#include "actor.h"
+
+bool actorInit(Actor *self, uint64_t id) {
+    // Set attributes
+    self->id = id;
+    // Set methods
+
+    return true;
+}
+
+uint64_t actorGetId(Actor *self) {
+    return self->id;
+}
---
+++ src/common/actor/actor.h
@@ -0,0 +1,26 @@
+/**
+ *
+ *   ██████╗   ██╗ ███████╗ ███╗   ███╗ ██╗   ██╗
+ *   ██╔══██╗ ███║ ██╔════╝ ████╗ ████║ ██║   ██║
+ *   ██████╔╝ ╚██║ █████╗   ██╔████╔██║ ██║   ██║
+ *   ██╔══██╗  ██║ ██╔══╝   ██║╚██╔╝██║ ██║   ██║
+ *   ██║  ██║  ██║ ███████╗ ██║ ╚═╝ ██║ ╚██████╔╝
+ *   ╚═╝  ╚═╝  ╚═╝ ╚══════╝ ╚═╝     ╚═╝  ╚═════╝
+ *
+ * @file commander_helpers.h
+ * @brief CommanderInfo contains all commander helpers functions
+ *
+ * @license GNU GENERAL PUBLIC LICENSE - Version 2, June 1991
+ *          See LICENSE file for further information
+ */
+
+#pragma once
+
+#include "R1EMU.h"
+
+typedef struct Actor {
+   uint64_t id;
+} Actor;
+
+bool actorInit(Actor *self, uint64_t id);
+uint64_t actorGetId(Actor *self);
